25ee1cc3042a7bff41340a89b68a42d9297cd720
Waitforngnl <nt998745@gmail.com>
2024-12-07 11:55:46 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿use master
+use users
+CREATE TABLE users
+(
+	id int primary key identity (1,1),
+	username varchar(max) null,
+	password varchar(max) null,
+	role varchar(max) null,
+	status varchar(max) null,
+	date_reg date null
+)
+
+select * from users
+INSERT INTO users (username, password, role, status, date_reg) Values ('admin', 'admin123', 'admin', 'Active', '2024-11-23')
+
+select * from users where role = 'Staff'
