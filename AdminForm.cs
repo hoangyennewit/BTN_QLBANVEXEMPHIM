14db256bdc7546e54db849f3001044b900239c97
hoangyennewit <144236166+hoangyennewit@users.noreply.github.com>
2024-12-13 23:22:01 +0700

    Add files via upload

---
+++ AdminForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLiBanVeXemPhim
+{
+    public partial class AdminForm : Form
+    {
+        public AdminForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Are you sure you want to exit?", "Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Are you sure you want to logout?", "Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                Form1 loginForm = new Form1();
+                loginForm.Show();
+
+                this.Hide();
+
+            }
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            dashboard1.Show();
+            frmAddStaffs1.Hide();
+        }
+
+        private void btnAddStaff_Click(object sender, EventArgs e)
+        {
+            frmAddStaffs1.Show();
+            dashboard1.Hide();
+        }
+
+        private void btnAddMovie_Click(object sender, EventArgs e)
+        {
+            addMovieForm1.Show();
+            frmAddStaffs1.Hide();
+        }
+    }
+}
